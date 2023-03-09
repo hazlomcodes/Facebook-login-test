9932cefce4f4de8608fbf15de1bc8328d7834c8b
hazlomcodes <harry.lomas1@outlook.com>
2023-03-09 11:27:30 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Facebook from './components/Facebook';
+
+function App() {
+  return (
+    <div className="App">
+      
+        <p>
+          Facebook Authentication
+        </p>
+      
+         <Facebook />
+       
+    </div>
+  );
+}
+
+export default App;
